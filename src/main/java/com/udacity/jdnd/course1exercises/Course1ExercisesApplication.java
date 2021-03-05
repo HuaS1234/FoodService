c909599bcc056067bec5854009b42c58f4c2ff27
Hua Shi <huashi2630@gmail.com>
2021-03-05 11:32:47 -0800

    0305added templates for homepage

--- src/main/java/com/udacity/jdnd/course1exercises/Course1ExercisesApplication.java
+++ src/main/java/com/udacity/jdnd/course1exercises/Course1ExercisesApplication.java
@@ -2,12 +2,18 @@ package com.udacity.jdnd.course1exercises;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Course1ExercisesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Course1ExercisesApplication.class, args);
+	}
+
+	@Bean
+	public String Message() {
+		return "Hello there";
 	}
 
 }
--- src/main/java/com/udacity/jdnd/course1exercises/lesson2/exercise1/FoodController.java
+++ src/main/java/com/udacity/jdnd/course1exercises/lesson2/exercise1/FoodController.java
@@ -4,22 +4,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Controller for receiving requests.
  */
-@RestController //combination of Controller + ResponseBody //Controller: receiving requests
-@RequestMapping("/food")
+//@ResponseBody //??ResponseBody doesn't support templates?????????
+@Controller  //combination of Controller + ResponseBody //Controller: receiving requests
+@RequestMapping//("/food")
 public class FoodController {
 
     private FoodService foodService;
+    private String message;
 
-    public FoodController(FoodService foodService) {
+    public FoodController(FoodService foodService, String message) {
+        this.message = message;
         this.foodService = foodService;
     }
-
 
     public String getHomePage(FoodForm foodForm, Model model) {
         foodService.addFood(foodForm.getFoodName(), foodForm.getCalories(), foodForm.getMealTime());
@@ -29,6 +32,16 @@ public class FoodController {
     public Boolean isFoodAvailableAtMealTime(@RequestBody String foodName) {
         MealTime mealTime = MealTime.BREAKFAST;
         return foodService.isFoodAvailableAtMealTime(foodName, mealTime);
+    }
+    @GetMapping
+    @RequestMapping("/home")  //need @RequestMapping or @GetMapping to response to the HTTP request.
+    public String getHomePage(Model m) {
+        //return message; //return "hello there" which was defined as bean at the main class.
+        m.addAttribute("welcomeMessage", Instant.now().toString()); //current timestamp
+        m.addAttribute("welcomeMessage", "test1"); //replace welcomeMessage
+        m.addAttribute("greetings", new String[]{"hi", "hello", "bye"});
+        m.addAttribute("msgs", "testn");
+        return "home";
     }
     @GetMapping
     public List<FoodData> getFoodRepository(@RequestBody int index) {
@@ -44,5 +57,4 @@ public class FoodController {
         String log = foodService.addFood(foodForm.getFoodName(), foodForm.getCalories(), foodForm.getMealTime());
         return foodForm.getFoodName() + log;
     }
-
 }
